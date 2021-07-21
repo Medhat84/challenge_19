dbaebd906ca16c0222fc0071242eb853e4f1767e
Medhat84 <med_mahanni@yahoo.com>
2021-07-22 00:51:45 +0200

    Submitted 22-July, 12:50 am

--- Challenge_19_2.R
+++ Challenge_19_2.R
@@ -27,8 +27,9 @@ for (i in 1:6){
   traintest <- traintest %>% mutate(wsav_dt1 = wsav - lag(wsav));traintest$wsav_dt1[1] <- 0; 
   traintest <- traintest %>% mutate(wsav_dt2 = wsav_dt1 - lag(wsav_dt1));traintest$wsav_dt2[1] <- 0; 
   traintest <- traintest %>% mutate(wsav_msd = rollapplyr(wsav, 4, sd, fill = 0));
-  #traintest <- traintest %>% mutate(wsv = wsav*vav);
+  traintest <- traintest %>% mutate(wp_ma49_ma8 = rollmeanr(wp_ma, k = 8, fill = 0, na.rm=TRUE));
   
+
   
   
   inTest <- which(is.na(traintest[,"wp"])); inValid <- inTest-length(testds$date);
@@ -39,24 +40,24 @@ for (i in 1:6){
   assign(paste0("wp",i,"valid"),valid);
   assign(paste0("wp",i,"test"),test);
   
-  mdl <- train(wp ~ ., method = "gbm", data = training, distribution = "laplace",
-               metric="MAE",verbose=FALSE,tuneGrid=expand.grid(n.trees=150,
-                                                               interaction.depth=4,
-                                                               shrinkage=0.1,
-                                                               n.minobsinnode=6));
+  #mdl <- train(wp ~ ., method = "gbm", data = training, distribution = "laplace",
+  #             metric="MAE",verbose=FALSE,tuneGrid=expand.grid(n.trees=150,
+  #                                                             interaction.depth=4,
+  #                                                             shrinkage=0.1,
+  #                                                             n.minobsinnode=6));
   
-  Valid_Pred <- predict(mdl, valid); 
-  Valid_Pred[Valid_Pred < 0] <- 0;Valid_Pred[Valid_Pred > 1] <- 1;
-  print(cbind(ValidMAE = MAE(Valid_Pred,valid$wp), mdl$results[7]));
-  assign(paste0("Valid_Pred",i),Valid_Pred);
+  #Valid_Pred <- predict(mdl, valid); 
+  #Valid_Pred[Valid_Pred < 0] <- 0;Valid_Pred[Valid_Pred > 1] <- 1;
+  #print(cbind(ValidMAE = MAE(Valid_Pred,valid$wp), mdl$results[7]));
+  #assign(paste0("Valid_Pred",i),Valid_Pred);
   #Test_Pred <- predict(mdl, test); 
   #Test_Pred[Test_Pred < 0] <- 0;Test_Pred[Test_Pred > 1] <- 1;
   #assign(paste0("Test_Pred",i),Test_Pred); 
   }
 
-Valid_Pred <- c(Valid_Pred1,Valid_Pred2,Valid_Pred3,Valid_Pred4,Valid_Pred5,Valid_Pred6);
-wp_valid <- c(wp1valid$wp,wp2valid$wp,wp3valid$wp,wp4valid$wp,wp5valid$wp,wp6valid$wp);
-print(MAE(Valid_Pred,wp_valid));
+#Valid_Pred <- c(Valid_Pred1,Valid_Pred2,Valid_Pred3,Valid_Pred4,Valid_Pred5,Valid_Pred6);
+#wp_valid <- c(wp1valid$wp,wp2valid$wp,wp3valid$wp,wp4valid$wp,wp5valid$wp,wp6valid$wp);
+#print(MAE(Valid_Pred,wp_valid));
 #Test_Pred <- cbind(Test_Pred1,Test_Pred2,Test_Pred3,Test_Pred4,Test_Pred5,Test_Pred6);
 #samp_sol <- read.csv("contribution_example.csv",sep = ";");
 #Result <- cbind(samp_sol$date,Test_Pred);
